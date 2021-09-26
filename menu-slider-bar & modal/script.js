4dceaa9ffbc64766f969b1875b89e809b1ec5007
Anh-Ngoc Le <87078780+ngocla99@users.noreply.github.com>
2021-09-26 21:22:30 +0700

    menu-slider-bar

---
+++ menu-slider-bar & modal/script.js
@@ -0,0 +1,17 @@
+'use strict';
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+// Toggle navigation
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('show-nav');
+});
+
+// Show and hide modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => {
+  e.target === modal ? modal.classList.remove('show-modal') : false;
+});
